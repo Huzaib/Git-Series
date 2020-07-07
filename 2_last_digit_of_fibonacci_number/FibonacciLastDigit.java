cae7b44f11436d0203690d227aa6730441cdbc96
Huzaib <huzaibulhassan@gmail.com>
2020-07-07 12:07:31 +0530

    First Commit

---
+++ 2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class FibonacciLastDigit {
+    private static long getFibonacciLastDigitNaive(int n) {
+        if (n <= 1)
+            return n;
+
+        long previous = 0;
+        long current  = 1;
+
+        for (int i = 0; i < n - 1; ++i) {
+            long tmp_previous = previous%10;
+            previous = current%10;
+            current = tmp_previous + current;
+        }
+		return current % 10;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+         
+        System.out.println(getFibonacciLastDigitNaive(n));
+    }
+}
+
